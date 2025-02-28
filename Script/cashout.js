0421bdf76b475d399148a727de0c659688d8c6d3
ayanstack94b <ayondevs.94b@gmail.com>
2025-02-28 11:47:30 +0530

    testing connection

--- Script/cashout.js
+++ Script/cashout.js
@@ -7,6 +7,16 @@ document.getElementById('cashOut-btn').addEventListener('click', function (event
     const amount = document.getElementById('cashout-amount').value;
     const convertedCashoutAmount = parseFloat(amount);
 
-    
+    const mainBalance = document.getElementById('main-balance').innerText;
+    const covertedMainBal = parseFloat(mainBalance);
+
+    if (convertedPin === 1234) {
+        const sum = covertedMainBal - convertedCashoutAmount;
+        console.log(sum)
+        const mainBalance = document.getElementById('main-balance').innerText = sum
+    }
+    else {
+        alert('enter valid pin')
+    }
 
 })
